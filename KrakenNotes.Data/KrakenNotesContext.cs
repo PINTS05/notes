ded7a1afdea34782d9aa5bdaca517c981fce1c4e
Artush Hakobyan <artush.hakobyan3@gmail.com>
2019-05-11 08:26:18 -0700

    add data layer

---
+++ KrakenNotes.Data/KrakenNotesContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KrakenNotes.Data
+{
+    public class KrakenNotesContext : DbContext
+    {
+        public KrakenNotesContext(DbContextOptions options) : base(options) { }
+
+
+    }
+}
