a6c8c55f4e951758548d0946beb8f684119ddf10
Artush Hakobyan <artush.hakobyan3@gmail.com>
2019-05-16 14:58:10 -0700

    add NotesRepository

---
+++ KrakenNotes.Services/Interfaces/INotesRepository.cs
@@ -0,0 +1,12 @@
+﻿using KrakenNotes.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KrakenNotes.Services.Interfaces
+{
+    public interface INotesRepository : IRepository<Note>
+    {
+        IEnumerable<Note> GetNotesByUserId(int id);
+    }
+}
---
+++ KrakenNotes.Services/NotesRepository.cs
@@ -0,0 +1,24 @@
+﻿using KrakenNotes.Data.Models;
+using KrakenNotes.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace KrakenNotes.Services
+{
+    public class NotesRepository : Repository<Note>, INotesRepository
+    {
+        public NotesRepository(DbContext context) : base(context)
+        {
+
+        }
+
+        public IEnumerable<Note> GetNotesByUserId(int id)
+        {
+            return GetAll().Where(e => e.Id == id).ToList();
+        }
+    }
+}
